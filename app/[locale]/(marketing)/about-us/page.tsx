466e25788b565944ba705dabba4f79801f55984c
arjunn2313 <arjunnks123@gmail.com>
2026-09-01 16:19:06 +0530

    hidden testimonial

--- app/[locale]/(marketing)/about-us/page.tsx
+++ app/[locale]/(marketing)/about-us/page.tsx
@@ -34,7 +34,7 @@ export default function AboutUsPage() {
       <TechnologyMeetsBeautySection/>
       <DoctorsSection/>
       <ClinicGallerySection/>
-      <TestimonialsSection/>
+      {/* <TestimonialsSection/> */}
       <FinalCtaSection/>
     
     </>
